ebf1bedeac294129fb0ce8985e62d302dbe42675
Wayne, W5XD. Round Rock <w5xd@writelog.com>
2024-11-27 02:12:15 +0000

    vs 2022

--- XDAudioDevice/XDVersion.h
+++ XDAudioDevice/XDVersion.h
@@ -1,6 +1,6 @@
 
-#define XDFILEVERSION 	1, 1, 0, 1
-#define XDPRODUCTVERSION 	1, 1, 0, 1
-#define XDFILEVERSIONSTR "1.1.0.1"
-#define XDPRODUCTVERSIONSTR "1.1.0.1"
-#define XDLEGALCOPYRIGHTSTR "Copyright (c) 2022 by Wayne E. Wright, W5XD"
+#define XDFILEVERSION 	1, 2, 0, 1
+#define XDPRODUCTVERSION 	1, 2, 0, 1
+#define XDFILEVERSIONSTR "1.2.0.1"
+#define XDPRODUCTVERSIONSTR "1.2.0.1"
+#define XDLEGALCOPYRIGHTSTR "Copyright (c) 2024 by Wayne E. Wright, W5XD"
